132299b2e4c52a7db5bfdcabef4b4f0dc3a06fa9
Makesh <65646841+makesh-git@users.noreply.github.com>
2021-11-11 16:35:06 +0530

    pushing http req changes

--- src/api.js
+++ src/api.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const request = require("request");
 
 const app = express();
 const router = express.Router();
-
+const getNewsData = () => {
+  request(
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=476849f56dfd431ba3c716068456306e",
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body); // Print the google web page.
+        return body;
+      }
+    }
+  );
+};
 router.get("/", (req, res) => {
   res.json({
-    hello: "hi!"
+    hello: getNewsData(),
   });
 });
 
