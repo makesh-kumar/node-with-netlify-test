d36cb80da05b9bd65bbb8be43b843619590b92af
Makesh <65646841+makesh-git@users.noreply.github.com>
2021-11-11 16:25:13 +0530

    removed prefix url

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
   });
 });
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
