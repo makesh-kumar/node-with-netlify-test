89d28e05d653e907a27003ea47169f8b31886dc4
Makesh <65646841+makesh-git@users.noreply.github.com>
2021-11-11 16:38:35 +0530

    changed request body

--- src/api.js
+++ src/api.js
@@ -5,12 +5,15 @@ const request = require("request");
 const app = express();
 const router = express.Router();
 const getNewsData = () => {
+  consiole.log("in getNewsData");
   request(
     "https://newsapi.org/v2/top-headlines?country=us&apiKey=476849f56dfd431ba3c716068456306e",
     function (error, response, body) {
-      if (!error && response.statusCode == 200) {
+      console.log("res came");
+      if (!error) {
+        console.log(response);
         console.log(body); // Print the google web page.
-        return body;
+        return response;
       }
     }
   );
